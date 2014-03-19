d45a8b802160ba25f969fad65ae6ad3ac502e4ad
Jonathan W Goodwin <jgoodwin@dataxu.com>
2014-03-18 23:55:08 -0400

    bash is now red, install script sucks even less

--- install.sh
+++ install.sh
@@ -7,14 +7,6 @@ else
   exit 1
 fi
 
-## TOTAL BULLSHIT !!!
-#brew tap mike-burns/rcm
-#brew install rcm
-#rcup -d ~/.dotfiles -x README.md -x LICENSE
-
-# Fuck that noise;
-# HERE WE GOOO
-
 ##########################################
 # Macs need things (they're also Darwin) #
 ##########################################
@@ -23,33 +15,37 @@ if [ $(uname) == 'Darwin' ];then
   # If its not... install brew stuff
 
   if brew -h &> /dev/null; then
-    echo "$(tput setaf 2)Warning:$(tput sgr 0) No brew; installing Homebrew..."
+    echo "$(tput setaf 2)Success:$(tput sgr 0) Homebrew already installed!"
+  else
+    echo "$(tput setaf 1)Warning:$(tput sgr 0) No brew; installing Homebrew..."
     # Install homebrew
+    set -x
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+    set +x
   fi
+
   # Then install our shits
   brew install \
     wget \
     fortune \
     cowsay \
-    vim
+    vim # Yes we install vim... basically the vim that ships on macos doesn't support my dotfiles (ansible)
 
   # Check if puppet is installed
-  if which puppet &> /dev/null; then gem install puppet; fi
-
+  if which puppet &> /dev/null; then echo "$(tput setaf 2)Success:$(tput sgr 0) puppet already installed!" ;else gem install puppet; fi
 fi
 
 #####################
 # OS Neutral Things #
 #####################
-if which puppet-lint &> /dev/null; then gem install puppet-lint; fi
+if which puppet-lint &> /dev/null; then echo "$(tput setaf 2)Success:$(tput sgr 0) puppet-lint already installed!" ;else gem install puppet-lint; fi
 
 
 # Install Dotfiles
 # Remove any dotfiles that we're clobbering
-ls |grep -v install | xargs -I {} rm -f ~/.{}
+ls |grep -v install | xargs -I {} rm -rf ~/.{}
 # Link in our own over those
-ls |grep -v install | xargs -I {} ln -s $wd/{} ~/.{}
+ls |grep -v install | xargs -I {} ln -s $(pwd)/{} ~/.{}
 
 # Install all bundles (vim bundler)
 vim +BundleInstall +qall
