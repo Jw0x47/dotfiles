d9b26cffb6c61123b5216a0778b97e66cec31238
Jonathan W Goodwin <jonathanwgoodwin@gmail.com>
2014-06-12 21:21:40 -0400

    xmodmap and vimpcrc.

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 xrandr --output HDMI-0 --auto --output DVI-0 --auto --right-of HDMI-0
-feh  --bg-scale '~/.dotfiles/backgrounds/dark_penguin_linux.png'
+feh  --bg-scale '/home/jgoodwin/.dotfiles/backgrounds/dark_penguin_linux.png'
 (xcompmgr &)
