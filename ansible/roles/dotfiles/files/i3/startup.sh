29c4363f327c374a35afa01ce22ef451b0d3a752
Jonathan W Goodwin <jonathanwgoodwin@gmail.com>
2014-09-26 17:16:58 -0400

    merged in arch changes

--- ansible/roles/dotfiles/files/i3/startup.sh
+++ ansible/roles/dotfiles/files/i3/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xrandr --output HDMI-0 --auto --output DVI-0 --auto --right-of HDMI-0
-feh  --bg-scale '/home/jgoodwin/.backgrounds/dark_penguin_linux.png'
+feh  --bg-scale '/home/jgoodwin/.config/backgrounds/dark_penguin_linux.png'
